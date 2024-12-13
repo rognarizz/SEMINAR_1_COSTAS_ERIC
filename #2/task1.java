bd71e8cab5d7c006eec5ac0bf774af60b54c42de
Costas <rgnrz1997@gmail.com>
2024-12-13 12:30:19 +0100

    Save

--- #2/task1.java
+++ #2/task1.java
@@ -2,28 +2,24 @@ public class task1 {
     public static void main(String[] args) {
         String basicURL = "https://example.com/search?";
         String params = "query=java&sort=desc&filter=null";
-        buildURL(basicURL, params);
+        String filter = "null";
+        buildURL(basicURL, params, filter);
     }
 
-    public static void buildURL(String baseURL, String params) {
+    public static void buildURL(String baseURL, String params, String filter) {
         StringBuilder url = new StringBuilder(baseURL);
         String[] pairs = params.split("&");
-    
+
         for (int i = 0; i < pairs.length; i++) {
             String[] keyValue = pairs[i].split("=");
-    
-            // Проверка, что значение не пустое и не равно строке "null"
-            if (keyValue.length > 1 && keyValue[1] != null && !keyValue[1].isEmpty()) {
-                // Добавление амперсанда, если это не первый параметр
+            if (!"null".equals(keyValue[1])) {
                 if (url.length() > baseURL.length()) {
                     url.append("&");
                 }
-                // Добавление параметра ключ-значение в URL
                 url.append(keyValue[0]).append("=").append(keyValue[1]);
             }
         }
-    
+
         System.out.println(url.toString());
     }
-    
 }
--- #2/task2.java
+++ #2/task2.java
@@ -1,3 +1,26 @@
-public class task2 {
-    
+class CSVGenerator {
+    public static String buildCSV(String[] head, String[][] data_elem) {
+        StringBuilder csv = new StringBuilder();
+
+        csv.append(String.join(",", head)).append("\n");
+
+        for (String[] row : data_elem) {
+            csv.append(String.join(",", row)).append("\n");
+        }
+        return csv.toString().trim();
+    }
+}
+
+public class task1 {
+    public static void main(String[] args) {
+        String[] headers = new String[]{"Name", "Age", "City"};
+        String[][] data = new String[][]{
+            {"John", "35", "New York"},
+            {"Alice", "25", "Los Angeles"},
+            {"Bob", "35", "Chicago"}
+        };
+
+        CSVGenerator csvGenerator = new CSVGenerator();
+        System.out.println(csvGenerator.buildCSV(headers, data));
+    }
 }
