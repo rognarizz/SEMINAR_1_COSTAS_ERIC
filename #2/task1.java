e668f43b3b55630053c96762d7970db32dbd86b3
Costas <rgnrz1997@gmail.com>
2024-12-13 12:03:03 +0100

    Save

--- #2/task1.java
+++ #2/task1.java
@@ -8,17 +8,22 @@ public class task1 {
     public static void buildURL(String baseURL, String params) {
         StringBuilder url = new StringBuilder(baseURL);
         String[] pairs = params.split("&");
-
+    
         for (int i = 0; i < pairs.length; i++) {
             String[] keyValue = pairs[i].split("=");
-            if (!"null".equals(keyValue[1])) {
+    
+            // Проверка, что значение не пустое и не равно строке "null"
+            if (keyValue.length > 1 && keyValue[1] != null && !keyValue[1].isEmpty()) {
+                // Добавление амперсанда, если это не первый параметр
                 if (url.length() > baseURL.length()) {
                     url.append("&");
                 }
+                // Добавление параметра ключ-значение в URL
                 url.append(keyValue[0]).append("=").append(keyValue[1]);
             }
         }
-
+    
         System.out.println(url.toString());
     }
+    
 }
